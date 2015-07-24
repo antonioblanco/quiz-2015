3733fcfb3d9d2e882fa10e7d5ecfc582196aa2f0
Antonio Blanco <ablancorojas@gmail.com>
2015-07-24 17:44:46 +0200

    nuevo commit
    nuevo commit

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+//GET /quizes/question
+exports.question = function(req, res){
+	res.render('quizes/question',{pregunta: 'Capital de Italia'});
+};
+
+//GET /quizes/answer
+exports.answer = function(req, res){
+	if (req.query.respuesta === 'Roma'){
+		res.render('quizes/answer',{respuesta: 'Correcta'});}
+	else{
+		res.render('quizes/answer',{respuesta: 'Incorrecta'});
+	}
+};
+
+exports.author = function(req, res){
+	res.render('author',{nombre: 'Antonio Blanco'});
+};
