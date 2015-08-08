3627212bcffd9fbc80e84d4b906433d19426f97e
Antonio Blanco <ablancorojas@gmail.com>
2015-08-08 17:54:38 +0200

    Update quiz_controller.js

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,8 +14,7 @@ exports.load = function(req, res, next, quizId){
 
 // GET /quizes
 exports.index = function(req, res) {  
-	var options= {};
-  models.Quiz.findAll(options).then(
+  models.Quiz.findAll().then(
     function(quizes) {
       res.render('quizes/index.ejs', {quizes: quizes});
     }
